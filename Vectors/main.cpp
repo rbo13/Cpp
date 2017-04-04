f141cde3a551de58429cd36a0f1034c5666b9859
whaangbuu <chardy.orofeo@yahoo.com.ph>
2017-04-04 23:12:23 +0800

    Added menu to Vector exercise

--- Vectors/main.cpp
+++ Vectors/main.cpp
@@ -54,7 +54,76 @@ void resize_vector() {
  * to the vector.
  */
 void vector_menu() {
-    
+
+    vector<int> vec;
+    vector<int>::iterator iter;
+    int choice, item;
+
+    while(1) {
+
+        cout<<"\n---------------------"<<endl;
+        cout<<"Vector Implementation in Stl"<<endl;
+        cout<<"\n---------------------"<<endl;
+        cout<<"1.Insert Element into the Vector"<<endl;
+        cout<<"2.Delete Last Element of the Vector"<<endl;
+        cout<<"3.Size of the Vector"<<endl;
+        cout<<"4.Display by Index"<<endl;
+        cout<<"5.Dislplay by Iterator"<<endl;
+        cout<<"6.Clear the Vector"<<endl;
+        cout<<"7.Exit"<<endl;
+        cout<<"Enter your Choice: ";
+        cin>>choice;
+
+        switch (choice) {
+
+            case 1:
+                cout << "Enter value to be inserted: ";
+                cin >> item;
+                vec.push_back(item);
+                break;
+
+            case 2:
+                cout << "Delete last item inserted: "<<endl;
+                vec.pop_back();
+                break;
+
+            case 3:
+                cout << "Size of vector is:";
+                cout << vec.size()<<endl;
+                break;
+
+            case 4:
+                cout << "Displaying elements by index: ";
+                for (int i = 0; i < vec.size(); ++i) {
+
+                    cout << vec[i] << " ";
+                }
+                cout << endl;
+                break;
+
+            case 5:
+                cout << "Display by iterator: ";
+
+                for (iter = vec.begin(); iter != vec.end(); iter++)
+                {
+                    cout<<*iter<<" ";
+                }
+                cout<<endl;
+                break;
+
+            case 6:
+                vec.clear();
+                cout << "Vector cleared!"<<endl;
+                break;
+
+            case 7:
+                exit(1);
+                break;
+
+            default:
+                cout << "Wrong choice!"<<endl;
+        }
+    }
 }
 
 int main() {
@@ -70,5 +139,6 @@ int main() {
     //create_vector();
     //resize_vector();
 
+    vector_menu();
     return 0;
 }
